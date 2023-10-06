2927a94bd19e9e39a79592e496358e5924eb0539
MP Anshuman (NTE-393) <mp.anshuman@nettantra.net>
2023-10-06 17:38:23 +0530

    added login and login required

--- django_blog/urls.py
+++ django_blog/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from user import views as user_views
+from django.contrib.auth.views import LoginView,LogoutView
 
 # userurlpatterns = [
 #     path("logintest/", user_views.login, name="user-login"),
@@ -10,6 +11,8 @@ from user import views as user_views
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("blog.urls")),
-    path("login/",user_views.login,name="user-login"),
+    path("profile/",user_views.profile,name="user-profile"),
+    path("login/",LoginView.as_view(template_name="user/login.html"),name="user-login"),
+    path("logout/",LogoutView.as_view(template_name="user/logout.html"),name="user-logout"),
     path("register/",user_views.register,name="user-register"),
 ]
--- user/views.py
+++ user/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from .forms import UserRegisterForm
+from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
@@ -24,3 +25,7 @@ def register(request):
     context = {"form": form}
 
     return render(request, "user/register.html", context)
+
+@login_required
+def profile(request):
+    return render(request, "user/profile.html")
