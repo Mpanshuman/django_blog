05ac59794840e94b6035e8efcb31f4c4e169da35
MP Anshuman (NTE-393) <mp.anshuman@nettantra.net>
2023-10-06 16:37:40 +0530

    added custom form

--- django_blog/urls.py
+++ django_blog/urls.py
@@ -2,15 +2,14 @@ from django.contrib import admin
 from django.urls import path, include
 from user import views as user_views
 
-userurlpatterns = [
-    path("logintest/", user_views.login, name="user-login"),
-    path("registertest/", user_views.register, name="user-register"),
-]
+# userurlpatterns = [
+#     path("logintest/", user_views.login, name="user-login"),
+#     path("registertest/", user_views.register, name="user-register"),
+# ]
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("blog.urls")),
     path("login/",user_views.login,name="user-login"),
     path("register/",user_views.register,name="user-register"),
-    path("user/", include(userurlpatterns)),
 ]
--- user/views.py
+++ user/views.py
@@ -1,29 +1,26 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from .forms import UserRegisterForm
+
 
 # Create your views here.
 def login(request):
-    form = UserCreationForm()
-    context = {
-        'form':form
-    }
-    if request.method == 'POST':
+    form = UserRegisterForm()
+    context = {"form": form}
+    if request.method == "POST":
         pass
-    return render(request, "user/login.html",context)
+    return render(request, "user/login.html", context)
 
 
 def register(request):
-    if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+    if request.method == "POST":
+        form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
-            messages.success(request, 'Account created')
-        return redirect('blog-home')
-    else:
-        form = UserCreationForm()
-        context = {
-            'form':form
-        }
-        return render(request, "user/register.html",context)
-    
+            messages.success(request, "Account created")
+            return redirect("blog-home")
+    form = UserRegisterForm()
+    context = {"form": form}
+
+    return render(request, "user/register.html", context)
