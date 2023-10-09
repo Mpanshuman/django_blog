b6a46c417af63d44648c02946b1a18dbba61e907
sonu 123 <m.p.ansuman10@gmail.com>
2023-10-10 00:10:12 +0530

    fixed save method throwing unexpected arg

--- user/models.py
+++ user/models.py
@@ -1,15 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import User
 from PIL import Image
+
+
 # Create your models here.
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    profile_pic = models.ImageField(default='default.jpg', upload_to='profile_pics')
+    profile_pic = models.ImageField(default="default.jpg", upload_to="profile_pics")
 
     def __str__(self):
         return self.user.username
-    
-    def save(self):
+
+    def save(self, **kwargs):
         super().save()
         img = Image.open(self.profile_pic.path)
         if img.height > 300 or img.width > 300:
