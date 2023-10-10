15bab2b082cd3ebe79539c09d2cb3f56c7f24cc6
sonu 123 <m.p.ansuman10@gmail.com>
2023-10-11 00:52:15 +0530

    added update and delete

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,8 @@ from . import views
 urlpatterns = [
     path("", views.index, name="blog-home"),
     path("about/", views.about, name="blog-about"),
-     path("post/<int:pk>/", views.PostDetailView.as_view(), name="blog-details"),
+    path("post/<int:pk>/", views.PostDetailView.as_view(), name="blog-details"),
+    path("post/new/", views.PostCreateView.as_view(), name="blog-create"),
+    path("post/<int:pk>/update/", views.PostUpdateView.as_view(), name="blog-update"),
+    path("post/<int:pk>/delete/", views.PostDeleteView.as_view(), name="blog-delete"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,56 @@
 from django.shortcuts import render
 from .models import Post
-from django.views.generic import CreateView,UpdateView,DeleteView,ListView,DetailView
+from django.views.generic import (
+    CreateView,
+    UpdateView,
+    DeleteView,
+    ListView,
+    DetailView,
+)
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+
 # Create your views here.
+
 
 class PostDetailView(DetailView):
     model = Post
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ["title", "body"]
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ["title", "body"]
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        if self.get_object().author == self.request.user:
+            return True
+        else:
+            return False
+
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = "/"
+
+    def test_func(self):
+        if self.get_object().author == self.request.user:
+            return True
+        else:
+            return False
+
+
 def index(request):
     posts = Post.objects.all()
     context = {"posts": posts}
